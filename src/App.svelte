61e2633b3e487a9c8b158ce05669d2bbb3b78b8d
Kevin Mason <krmason2020@gmail.com>
2022-05-24 18:07:56 -0700

    onto https://www.youtube.com/watch?v=U8cM_jhmDxA&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=20&ab_channel=TheNetNinja

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
-  export let name;
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-</main>
+<Header />
+<main />
+<Footer />
 
 <style>
+  main {
+    max-width: 60rem;
+    margin: 2.5rem auto;
+  }
 </style>
