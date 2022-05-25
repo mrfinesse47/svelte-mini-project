e440871c633387d6d01329ab92d854df40473677
Kevin Mason <krmason2020@gmail.com>
2022-05-24 23:00:55 -0700

    onto https://www.youtube.com/watch?v=OC_WmW3nZCs&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=25&ab_channel=TheNetNinja

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import PollList from "./components/PollList.svelte";
   import CreatePollForm from "./components/CreatePollForm.svelte";
   import Tabs from "./shared/Tabs.svelte";
   //tabs
@@ -9,15 +10,33 @@
   const tabChange = (e) => {
     activeItem = e.detail.item;
   };
+  let polls = [
+    {
+      id: 1,
+      question: "python or JS",
+      answerA: "js",
+      answerB: "python",
+      votesA: 9,
+      votesB: 15,
+    },
+  ];
+
+  $: console.log(polls);
+
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    activeItem = "Current Polls";
+  };
 </script>
 
 <Header />
 <main>
   <Tabs {items} {activeItem} on:tabchange={tabChange} />
   {#if activeItem === "Current Polls"}
-    <p>poll list comp goes here</p>
+    <PollList {polls} />
   {:else if activeItem === "Add New Poll"}
-    <CreatePollForm />
+    <CreatePollForm on:add={handleAdd} />
   {/if}
 </main>
 <Footer />
