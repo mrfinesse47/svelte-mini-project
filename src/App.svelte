fce9dadc1c66bb4ea9e9285141de237554bd319b
Kevin Mason <krmason2020@gmail.com>
2022-05-24 19:34:20 -0700

    onto https://www.youtube.com/watch?v=Cv8vo4ZBy2E&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=21&ab_channel=TheNetNinja

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+  //tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+  const tabChange = (e) => {
+    activeItem = e.detail.item;
+  };
 </script>
 
 <Header />
-<main />
+<main>
+  <Tabs {items} {activeItem} on:tabchange={tabChange} />
+  {#if activeItem === "Current Polls"}
+    <p>poll list comp goes here</p>
+  {:else if activeItem === "Add New Poll"}
+    <p>add new poll comp goes here</p>{/if}
+</main>
 <Footer />
 
 <style>
